97246b49efdc0fc913d55fbb09b0473121a4b71f
muperdev <muperdev@gmail.com>
2023-11-19 16:11:12 +0400

    feat: change cors setting

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-require('dotenv').config();
-const express = require('express');
-const axios = require('axios');
-const HttpsProxyAgent = require('https-proxy-agent');
-const cors = require('cors');
-const { createProxyMiddleware } = require('http-proxy-middleware');
+require("dotenv").config();
+const express = require("express");
+const axios = require("axios");
+const HttpsProxyAgent = require("https-proxy-agent");
+const cors = require("cors");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const app = express();
 app.use(cors());
@@ -11,38 +11,32 @@ app.use(cors());
 const port = process.env.PORT || 8081;
 
 const corsOpts = {
-  origin: '*',
-  
-  
-  
-  methods: [
-  'GET',
-  'POST',
+  origin: "*",
+  methods: ["GET", "POST"],
+  headers: [
+    'Content-Type',
+    'Authorization',
+    'application/json',
+    'text/plain',
+    '*/*',
   ],
-  
-  
-  
-  allowedHeaders: [
-  'Content-Type',
-  ],
-  };
-  
-  
-  
-  app.use(cors(corsOpts));
+  maxAge: 3600,
+};
+
+app.use(cors(corsOpts));
 
 app.use(express.json());
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 const HUNTER_API_KEY = process.env.HUNTER_API_KEY;
 
 let awaitingDomain = false;
-let potentialName = '';
+let potentialName = "";
 let conversationHistory = [];
 
-app.post('/api/query', async (req, res) => {
+app.post("/api/query", async (req, res) => {
   try {
     const userQuery = req.body.query;
-    addToConversationHistory('User', userQuery);
+    addToConversationHistory("User", userQuery);
 
     if (awaitingDomain) {
       const domain = userQuery.trim();
@@ -68,14 +62,14 @@ app.post('/api/query', async (req, res) => {
       if (nameMatch && nameMatch[1]) {
         potentialName = nameMatch[1].trim();
         awaitingDomain = true;
-        res.json({ message: 'Sure, can you provide the domain?' });
+        res.json({ message: "Sure, can you provide the domain?" });
       } else {
-        res.json({ message: 'Please provide a full name in your query.' });
+        res.json({ message: "Please provide a full name in your query." });
       }
     } else {
       const conversationPrompt = generateSalesConversationPrompt(userQuery);
       const openaiResponse = await axios.post(
-        'https://api.openai.com/v1/engines/text-davinci-003/completions',
+        "https://api.openai.com/v1/engines/text-davinci-003/completions",
         {
           prompt: conversationPrompt,
           max_tokens: 150,
@@ -88,66 +82,76 @@ app.post('/api/query', async (req, res) => {
       );
 
       const botResponse = openaiResponse.data.choices[0].text.trim();
-      addToConversationHistory('Bot', botResponse);
+      addToConversationHistory("Bot", botResponse);
 
       res.json({ message: botResponse }); // Send the response as JSON
     }
   } catch (error) {
-    console.error('Error:', error.response || error);
-    res.status(500).json({ message: 'An error occurred.', error: error.response || error });
+    console.error("Error:", error.response || error);
+    res
+      .status(500)
+      .json({ message: "An error occurred.", error: error.response || error });
   }
 });
 
 function addToConversationHistory(role, message) {
   conversationHistory.push({ role, message });
   if (conversationHistory.length > 20) {
-      conversationHistory.shift();
+    conversationHistory.shift();
   }
 }
 
 async function makeRequestThroughProxy(url) {
-  const proxyAgent = new HttpsProxyAgent('https://archivebackend-bdf93cdd1c16.herokuapp.com/');
+  const proxyAgent = new HttpsProxyAgent(
+    "https://archivebackend-bdf93cdd1c16.herokuapp.com/"
+  );
 
   try {
-      const response = await axios.get(url, { httpsAgent: proxyAgent });
-      console.log(response.data);
-      return response.data;
+    const response = await axios.get(url, { httpsAgent: proxyAgent });
+    console.log(response.data);
+    return response.data;
   } catch (error) {
-      console.error('Error making request through proxy:', error);
-      throw error; // Rethrow the error for further handling
+    console.error("Error making request through proxy:", error);
+    throw error; // Rethrow the error for further handling
   }
 }
 
 function generateSalesConversationPrompt(userQuery) {
   const recentHistory = conversationHistory
-      .slice(-20)
-      .map(entry => `${entry.role}: ${entry.message}`)
-      .join('\n');
+    .slice(-20)
+    .map((entry) => `${entry.role}: ${entry.message}`)
+    .join("\n");
   return `The following is a conversation with an AI sales assistant specializing in writing sales emails, strategizing sales, and creating sales campaigns.\n${recentHistory}\nHuman: ${userQuery}\nAI:`;
 }
 
-app.get('/api/conversation-history', (req, res) => {
+app.get("/api/conversation-history", (req, res) => {
   res.json({ history: conversationHistory });
 });
 
-app.use('/api', createProxyMiddleware({
-  target: 'https://archiveapp-53952ecc1091.herokuapp.com/',
-  changeOrigin: true,
-  secure: true,
-  pathRewrite: {
-  '^/api': '',
-  },
-  onProxyRes: (proxyRes, req, res) => {
-  // Set CORS headers in the proxy response
-  res.setHeader('Access-Control-Allow-Origin', 'https://archiveapp.vercel.app/');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  },
-  }));
+app.use(
+  "/api",
+  createProxyMiddleware({
+    target: "https://archiveapp-53952ecc1091.herokuapp.com/",
+    changeOrigin: true,
+    secure: true,
+    pathRewrite: {
+      "^/api": "",
+    },
+    onProxyRes: (proxyRes, req, res) => {
+      // Set CORS headers in the proxy response
+      res.setHeader(
+        "Access-Control-Allow-Origin",
+        "*"
+      );
+      res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET,HEAD,PUT,PATCH,POST,DELETE"
+      );
+    },
+  })
+);
 
-
-
-app.listen(port, '0.0.0.0', () => {
+app.listen(port, "0.0.0.0", () => {
   console.log(`Server is running on port ${port}`);
-  console.log('OpenAI API Key:', process.env.OPENAI_API_KEY);
+  console.log("OpenAI API Key:", process.env.OPENAI_API_KEY);
 });
